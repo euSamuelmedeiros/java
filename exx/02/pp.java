68a52419cd4efae5ba37fab79465f2731189e8d0
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-04-21 19:10:49 -0300

    primeiros passos em java

---
+++ exx/02/pp.java
@@ -0,0 +1,8 @@
+public class pp {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Ola, mundo, estou dando meus primeiros passos em java.");
+    }
+}
+
+// comentario de uma linha
+/*comentario de multiplas linhas */
