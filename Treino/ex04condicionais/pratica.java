e7764ae688c416fbbb38b475fbed0eef0d00cd6d
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-04-30 00:49:16 -0300

    :0

--- Treino/ex04condicionais/pratica.java
+++ Treino/ex04condicionais/pratica.java
@@ -5,20 +5,6 @@ import java.util.Scanner;
 public class pratica {
     public static void main(String[] args) {
         /*
-         * Scanner input = new Scanner(System.in);
-         * System.out.print("Qual é a sua idade? ");
-         * int age = input.nextInt();
-         * if (age < 15) {
-         * System.out.println("sua categoria é a infantil");
-         * } else if (age >= 15 && age < 18) {
-         * System.out.println("sua categoria é a juvenil");
-         * } else {
-         * System.out.println("sua categoria é a dos adultos");
-         * }
-         * 
-         * input.close();
-         */
-
         Scanner input = new Scanner(System.in);
         System.out.println("Qual a sua primeira nota? ");
         float notice1 = input.nextFloat();
@@ -40,6 +26,22 @@ public class pratica {
         input.close();
 
         input.close();
+
+         /*
+         * Scanner input = new Scanner(System.in);
+         * System.out.print("Qual é a sua idade? ");
+         * int age = input.nextInt();
+         * if (age < 15) {
+         * System.out.println("sua categoria é a infantil");
+         * } else if (age >= 15 && age < 18) {
+         * System.out.println("sua categoria é a juvenil");
+         * } else {
+         * System.out.println("sua categoria é a dos adultos");
+         * }
+         * 
+         * input.close();
+         */
+
     }
 
 }
---
+++ aulas/aula08/arrays02.java
@@ -0,0 +1,15 @@
+package aula08;
+
+public class arrays02 {
+    public static void main(String[] args) {
+        String[] name = new String[5];
+        name[0] = "samuca";
+        name[1] = "muca";
+        name[2] = "malu";
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+
+        }
+
+    }
+}
