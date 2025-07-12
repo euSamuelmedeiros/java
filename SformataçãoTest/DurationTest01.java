d05a78a2d8c07952a2f547179c5d8b768d458e00
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-07-12 01:33:10 -0300

    ;)

--- SformataçãoTest/DurationTest01.java
+++ SformataçãoTest/DurationTest01.java
@@ -1,11 +1,10 @@
 package SformataçãoTest;
 
 
-import java.time.LocalDateTime;
 
 public class DurationTest01 {
     public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime nowAFtertwoYear = LocalDateTime.now().plusYears(2);
+        // LocalDateTime now = LocalDateTime.now();
+        // LocalDateTime nowAFtertwoYear = LocalDateTime.now().plusYears(2);
     }
 }
---
+++ TResourceBundle/Test/ResourceBundleTest01.java
@@ -0,0 +1,13 @@
+package TResourceBundle.Test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTest01 {
+    public static void main(String[] args) {
+      System.out.println(Locale.getDefault());
+      ResourceBundle bundle =  ResourceBundle.getBundle("message", new Locale("pt", "BR"));
+      
+    }
+
+}
