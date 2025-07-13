9db5907cf57c925f1112e1f9f251e359614a5f1e
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-07-13 00:09:02 -0300

    .

--- TResourceBundle/Test/ResourceBundleTest01.java
+++ TResourceBundle/Test/ResourceBundleTest01.java
@@ -7,7 +7,7 @@ public class ResourceBundleTest01 {
     public static void main(String[] args) {
       System.out.println(Locale.getDefault());
       ResourceBundle bundle =  ResourceBundle.getBundle("message", new Locale("pt", "BR"));
-      
+    
     }
 
 }
